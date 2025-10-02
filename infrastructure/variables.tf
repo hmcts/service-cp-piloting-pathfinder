55055624360a5cdc00e091e1d73c1e66298b416e
coling01 <colin.greenwood@hmcts.net>
2025-10-02 18:09:02 +0100

    Ready

---
+++ infrastructure/variables.tf
@@ -0,0 +1,48 @@
+variable "product" {
+  type    = string
+  default = "ccpay"
+}
+
+variable "component" {
+  type    = string
+  default = "bulkscanning-api"
+}
+
+variable "product_name" {
+  type    = string
+  default = "bulk-scanning-payment"
+}
+
+variable "location_app" {
+  type    = string
+  default = "UK South"
+}
+
+variable "env" {
+  type = string
+}
+
+variable "subscription" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map(string)
+}
+
+# thumbprint of the SSL certificate for API gateway tests
+variable "bulkscanning_api_gateway_certificate_thumbprints" {
+  type    = list(string)
+  default = []
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "aks_subscription_id" {}
+
+variable "apim_suffix" {
+  default = ""
+}
+
